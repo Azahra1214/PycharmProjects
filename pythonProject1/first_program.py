fb2044970791d8315f6b8ce1cc45c549e2389b82
zahra2 <andleeb.zahra@postgrad.manchester.ac.uk>
2023-12-19 17:29:24 +0000

    first commit

--- pythonProject1/first_program.py
+++ pythonProject1/first_program.py
@@ -1,3 +1,29 @@
-print("hello world")
-print("hello")
-python --version
+#slicing
+a="hello world"
+print(a[-5:-2])
+#upper and lowe case methods
+a="hello world"
+print(a.upper())
+b="i am zahra"
+print(b.lower())
+#replacing a string
+a="Hello world!"
+print(a.replace("H","j"))
+#split string
+a="Hello, World"
+print(a.split(","))
+#string concat
+a="hello"
+b="world"
+c=a+b
+print(c)
+#string formatting
+age=36
+txt="my name is jhon and i am {}"
+print(txt.format(age))
+#format argument
+quantity=3
+itemno=555
+price=50
+my_order="i want {} pieces of item{} for {} dollers"
+print(my_order.format(quantity,itemno,price))
