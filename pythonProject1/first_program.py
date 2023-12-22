e1b66c88b5a681063e4a888d0a01db43a16ce016
zahra2 <andleeb.zahra@postgrad.manchester.ac.uk>
2023-12-22 06:45:06 +0000

    first commit

--- pythonProject1/first_program.py
+++ pythonProject1/first_program.py
@@ -1,3 +1,105 @@
+# This is a sample Python script.
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+print("hello world")
+#this is a comment
+#python identation excercise
+if 5>2:
+    print("five is greater than two")
+    if 5>2:
+        print("five is greater")
+        print("five is greater than two")
+
+
+#Python variable
+x=5
+y="hello world"
+print(x)
+print(y)
+
+#indentation
+if 5>2:
+    print("yes")
+#assigning a variable
+carname="volvo"
+print (carname)
+
+x=5
+y=10
+print(x+y)
+
+#getting the char from index 2:4
+txt="hello world"
+x=txt[2:4]
+
+#type of variables
+x=5
+y="jhon"
+print(type(x))
+print(type(y))
+
+#Many values of multiple variables
+x,y,z="orange","banana","cherry"
+print(x)
+print(y)
+print(z)
+
+#unpacking a List
+x,y,z=["apples","banana","oragne"]
+print(x)
+print(y)
+print(z)
+"""global variables
+declear variable outside and call in function"""
+x="awesome"
+def myfunc():
+    print("my python is "+x)
+myfunc()
+
+"""creating variable inside function 
+as global variable"""
+x="awesome"
+def myfunc():
+    x="fantastic"
+    print("python is "+x)
+myfunc()
+print("python is very "+x)
+#global keyword
+def myfunc():
+    global x
+    x="good"
+myfunc()
+print("python is "+x)
+#print randon range
+import random
+print(random.randrange(1,10))
+#multiline string
+a="""this is 
+a miltiline string"""
+print(a)
+a="hello world"
+print(a[2])
+#Looping through array
+for x in "banana":
+    print(x)
+#string Length
+a="hello world"
+print(len(a))
+#check string
+a="the best in life is free"
+print("free" in a)
+if "free" in a:
+    print("yes free is present")
+
 #slicing
 a="hello world"
 print(a[-5:-2])
@@ -29,3 +131,10 @@ my_order="i want {} pieces of item{} for {} dollers"
 print(my_order.format(quantity,itemno,price))
 #boolean operator
 print (9<10)
+
+def func():
+    return True
+if func():
+    print("yes")
+else:
+    print("no")
